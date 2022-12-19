f1815219944c4246ac38e93d7e97ed7d467b143c
Stefan Shambrook <stefshambrook@gmail.com>
2022-12-19 15:09:48 +0000

    Add createGrid() and styles to build grid

---
+++ main.js
@@ -0,0 +1,19 @@
+function createGrid() {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    const cellCount = 16;
+
+    for (let i = 0; i < cellCount; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < cellCount; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+    document.body.insertBefore(grid, document.querySelector('.button-container'));
+}
+
+createGrid();
